f3e31c99205cc6c1765a90796cdfd1110a18b946
Andrew Rob <andrew-c-rob@live.com>
2018-05-02 17:01:20 -0500

    Wrote a working solution that passes all tests currently

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,3 +1,17 @@
-var reverseString = function() {
-
+//inputs: 1 string
+//outputs: 1 string
+//process: 'never' => 'reven'
+var reverseString = function(str) {
+	var arr = [];
+	//iterate through inputs str starting at last char and ending at first char
+	for(var i = str.length - 1; i >= 0; i--) {
+		//push element into arr
+		arr.push(str[i]);
+	}
+	//join elements in array without commas into a string
+	var revStr = arr.join('');
+	//return string
+	return revStr;
 };
+
+//console.log(reverseString('blow'));
