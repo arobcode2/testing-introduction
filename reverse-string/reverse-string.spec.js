ce7ed66397e8316258b78b56c61380c1a6458b65
Andrew Rob <andrew-c-rob@live.com>
2018-05-02 16:49:27 -0500

    Wrote tests for reverseString

--- reverse-string/reverse-string.spec.js
+++ reverse-string/reverse-string.spec.js
@@ -2,4 +2,13 @@ describe('reverseString', function() {
   it('reverses a string', function() {
     expect(reverseString('never')).to.equal('reven');
   });
+  it('returns same character if 1 char is passed in for a string', function() {
+  	expect(reverseString('a')).to.equal('a');
+  });
+  it('returns an empty string if an empty string is passed in', function() {
+  	expect(reverseString('')).to.equal('');
+  });
+  it('length of reversed string is equal to passed in string', function() {
+  	expect(reverseString('yes')).to.equal(3);
+  });
 });
