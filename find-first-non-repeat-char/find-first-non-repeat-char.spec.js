ff56b0d95a8971a8bf40b7793e8df10e743a391b
Andrew Rob <andrew-c-rob@live.com>
2018-05-02 17:20:02 -0500

    Wrote tests for firstNonRepeatingChar

--- find-first-non-repeat-char/find-first-non-repeat-char.spec.js
+++ find-first-non-repeat-char/find-first-non-repeat-char.spec.js
@@ -1,3 +1,11 @@
 describe('firstNonRepeatChar', function() {
-
+	it('finds the first non repeating character in a string', function() {
+		expect(firstNonRepeatChar('abacabad')).to.equal('c');
+	});
+	it('returns null if a string has characters which all repeat', function() {
+		expect(firstNonRepeatChar('aabb')).to.equal(null);
+	});
+	it('returns an empty string if an empty string is passed in', function() {
+		expect(firstNonRepeatChar('')).to.equal('');
+	});
 });
